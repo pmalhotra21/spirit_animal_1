7910020a47da3591cc2be83c07101b3d2b0769bb
pmalhotra21 <pmalhotra1@una.edu>
2024-09-28 23:34:39 -0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
+console.log("in console...");
+
+$('#btnToggle').on('click', function() {
+  console.log("event?:", "toggle clicked");
+
+  $('#divGameSection').toggle(); 
+
+});
+
+$('#divGameSection').draggable(); 
+
+$('#imgDrag').draggable(); 
+
 // $('body').css("background-color", "yellow");
 
 // when button is clicked
@@ -7,13 +20,14 @@ $("#btnGetAnimal").on("click", function(){
   let bakerName = $("#bakerName").val(); 
   //var variableName =  ... let (const) 
   
- $("#greetingArea").text("Hi there, " + bakerName + "!");  
+  $("#greetingArea").text("Hi there, " + bakerName + "!");  
   //give them a personalized greeting
   
   let randomAnimal = spiritAnimals[(Math.floor(Math.random() * spiritAnimals.length))];
   
-  $("#greetingArea").text("Your spirit animal is: " + randomAnimal)
+  $("#animalArea").text("Your spirit animal is: " + randomAnimal)
   //next, also tell spirit animal 
 } )
+
 
 let spiritAnimals = ["🦄","🦌","🐎","🦮","🐧","🐇","🐳","🐩","file not found"]
